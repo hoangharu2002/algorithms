51d66bc4d983b1a4f98a90def8c29c439e952a99
Just4Func <nnhoang2002@gmail.com>
2023-05-24 00:13:30 +0700

    add method 1

--- Swap value/.cpp
+++ Swap value/.cpp
@@ -2,12 +2,18 @@
 
 void SwapMethod1(int &a, int &b){
     //The idea is using another variable to keep one of two value
+    int temp = a;
+    a = b;
+    b = temp;
 }
 
 int main() {
     int a = 1, b = 2;
     //Print two value
     std::cout << "a = " << a << " and b = " << b;
+
+    //Method 1
+    SwapMethod1(a, b);
 
     //Print result
     std::cout << "a = " << a << " and b = " << b;
