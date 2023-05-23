2e14e17af7d70b3161d5ed05efb1f6e6eb03159e
Just4Func <nnhoang2002@gmail.com>
2023-05-23 23:56:20 +0700

    Create main

---
+++ Swap value/.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+void SwapMethod1(int &a, int &b){
+    //The idea is using another variable to keep one of two value
+}
+
+int main() {
+    int a = 1, b = 2;
+
+    //Print result
+    std::cout << "a = " << a << " and b = " << b;
+
+    return 0;
+}
