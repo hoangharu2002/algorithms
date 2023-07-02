56f2f4552703e4442cd6c044c1355278b0cb750d
Just4Func <nnhoang2002@gmail.com>
2023-07-02 21:06:27 +0700

    create 0 array

---
+++ create array/0_array.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+
+//show array
+void showArray(int *arr, int n){
+    std::cout << "Mang cua ban la: \n";
+    for (int i = 0; i < n; ++i)
+        std::cout << arr[0] << " ";
+}
+
+int main(){
+    //create 0 array method 1
+    int arr1[10] = {0};
+
+    //create 0 array
+    int arr2[10];
+    for (int i = 0; i < 10; ++i){
+        arr2[i] = 0;
+    }
+
+    //show array
+    showArray(arr1, 10);
+    return 0;
+}
